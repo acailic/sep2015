5ae3479cd745ae349d766eb8afecc3687c27a8b5
laurafarkas <anonimna-@hotmail.com>
2015-12-12 14:36:38 +0100

    ubacen ui-view u wizard za prodaju

--- projekat/app/components/sale/sale.route.js
+++ projekat/app/components/sale/sale.route.js
@@ -9,10 +9,34 @@
 	function config($stateProvider) {
 		$stateProvider
 			.state('main.sale', {
-				url: '/sale',
+				abstract: true,
 				views: {
 					'content@': {
-						templateUrl: 'app/components/sale/sale.html',
+						templateUrl: 'app/components/sale/sale.html'
+					}
+				}
+			})
+			.state('main.sale.wizard1', {
+				url: '/saleWizard1',
+				views: {
+					'wizard': {
+						templateUrl: 'app/components/sale/sale-wizard-part1.html'
+					}
+				}
+			})
+			.state('main.sale.wizard2', {
+				url: '/saleWizard2',
+				views: {
+					'wizard': {
+						templateUrl: 'app/components/sale/sale-wizard-part2.html'
+					}
+				}
+			})
+			.state('main.sale.wizard3', {
+				url: '/saleWizard3',
+				views: {
+					'wizard': {
+						templateUrl: 'app/components/sale/sale-wizard-part3.html',
 						controller: 'SaleController',
 						controllerAs: 'slc'
 					}
