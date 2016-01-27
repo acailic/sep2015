272275152131527435af4df9f87880692407f9ee
IgorJovin <igoryov@gmail.com>
2016-01-27 15:59:01 +0100

    CORS

--- spring4-mvc-gradle-xml/src/main/java/org/sep/merchant/form/util/SimpleCORSFilter.java
+++ spring4-mvc-gradle-xml/src/main/java/org/sep/merchant/form/util/SimpleCORSFilter.java
@@ -31,8 +31,7 @@ public class SimpleCORSFilter  implements Filter {
 	    response.setHeader("Access-Control-Allow-Credentials", "true");
 	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 	    response.setHeader("Access-Control-Max-Age", "3600");
-	    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-
+	    response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 	    chain.doFilter(req, res);
 	}
 
