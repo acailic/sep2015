fa1558564949ff5f911fac1a827acb862b4ccd6d
danijelaaaaa <danijelaaaaa7@gmail.com>
2016-02-02 15:55:45 +0100

    Selektovanje tabova u okviru menija

--- projekat/app/components/core/core.module.js
+++ projekat/app/components/core/core.module.js
@@ -2,5 +2,44 @@
 	"use strict";
 	
 	angular
-		.module('merchant.core', ['ui.router']);
+		.module('merchant.core', ['ui.router'])
+		.directive('myDirective',function() {
+  			return {
+  				
+  				restrict: 'A',
+  				
+			    link: function link(scope, element, attrs){
+
+			     scope.$on('$stateChangeStart', 
+					function(event, toState, toParams, fromState, fromParams){ 
+
+					if(toState.name === "main.insurance"){
+						scope.selected = 0;
+
+					}
+
+
+					if(toState.name === "main.calculator"){
+			    		scope.selected = 1;
+			    	}
+
+			    	
+			    	if(toState.name === "main.sale.wizard1"){
+			    		scope.selected = 2;
+			    	}
+
+
+			    	if(toState.name === "main.about"){
+			    		scope.selected = 3;
+			    	}
+
+
+
+					});
+			      
+			    
+
+			    }
+  			};
+		});
 })();
